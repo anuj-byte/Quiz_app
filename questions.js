723bfa57f08c37e16ceccc857e9089ae53176553
anuj singh <84653962+anuj-byte@users.noreply.github.com>
2022-08-02 00:11:31 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The Battle of Plassey was fought in",
+    answer: "1757",
+    options: [
+      "1757",
+      "1782",
+      "1748",
+      "1764"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The territory of Porus who offered strong resistance to Alexander was situated between the rivers of",
+    answer: "Jhelum and Chenab",
+    options: [
+      "Sutlej and Beas",
+      "Jhelum and Chenab",
+      "Ravi and Chenab",
+      "Ganga and Yamuna"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Under Akbar, the Mir Bakshi was required to look after",
+    answer: "military affairs",
+    options: [
+      "military affairs",
+      "the state treasury",
+      "the royal household",
+      "the land revenue system"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Tripitakas are sacred books of",
+    answer: "	Buddhists",
+    options: [
+      "	Buddhists",
+      "Hindus",
+      "	Jains",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "	The trident-shaped symbol of Buddhism does not represent",
+    answer: "	Nirvana",
+    options: [
+      "	Nirvana",
+      "	Sangha",
+      "Buddha",
+      " Dhamma"
+    ]
+  },
+]
